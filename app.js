9e98895076fb41e8323c33af1902099a27fa36d9
tolik555 <tolik555.83@gmail.com>
2017-04-12 19:43:06 +0300

    Подключены модули express body-parser cllc Написан сервер

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 /**
  * Created by ktolik on 12.04.2017.
  */
+var express = require('express');
+var bodyParser = require('body-parser');
+var cllc = require('cllc')();
+
+var app = express();
+
+var server = app.listen(3000, function () {
+    console.log('Listening on port %d', server.address().port);
+});
